0e09fb93c2a3479fcf4707f22c54f6b65c2069b4
melvinleroux <leroux.melvin@gmail.com>
2024-06-28 18:32:00 +0200

    creation navbar and starting article page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiquesComponent} from "./pages/page-statistiques/page-statistiques.component";
+import {PageArticleComponent} from "./pages/articles/page-article/page-article.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : 'login',
+    component : PageLoginComponent
+  },
+  {
+    path: 'inscription',
+    component : PageInscriptionComponent
+  },
+  {
+    path: '',
+    component : PageDashboardComponent,
+    children:  [
+      {
+        path : 'statistiques',
+        component : PageStatistiquesComponent
+      },
+      {
+        path: 'articles',
+        component: PageArticleComponent
+
+      }
+    ]
+  }
+];
